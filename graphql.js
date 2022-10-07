22a42817982b0fd353aaa77963a2cdf444714836
cabanaquinta <alexroperez3@gmail.com>
2022-10-07 02:00:29 +0200

    now

--- graphql.js
+++ graphql.js
@@ -1,8 +1,8 @@
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: "http://localhost:1337/graphql",
-    // uri: process.env.NEXT_PUBLIC_BACKEND_API,
+    // uri: "http://localhost:1337/graphql",
+    uri: process.env.NEXT_PUBLIC_BACKEND_API,
     // headers: {
     //     Authorization: `Bearer 4c48dae1021a63058b304fb4ee4bcdd3e7382b5d2f3c532e321f9d06a9e8d8af20d46155d9bec64d987eb904181517f8d297b7924ae939da037bdedf075e563fcb5b7fc7d2870b50d4a7d7ac65009fa5138262163ec76dc112692e9606f4f793fe2669a8cede752e780f86bde950bfda720c67d79f0e292c351a82488f2b45f1`,
     // },
